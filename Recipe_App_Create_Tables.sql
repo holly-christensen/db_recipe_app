94daf6b2bb7c060db254610d2ecbf443b506c439
Holly Christensen <holly.christensen@comcast.net>
2020-11-30 18:55:50 -0500

    Added University table to database

--- Recipe_App_Create_Tables.sql
+++ Recipe_App_Create_Tables.sql
@@ -4,6 +4,11 @@ create database recipeapp;
 
 use recipeapp;
 
+drop table if exists University;
+create table University (
+	university_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    university_name VARCHAR(50) NOT NULL
+);
 
 drop table if exists User;
 create table User (
@@ -15,13 +20,14 @@ create table User (
     password VARCHAR(50) NOT NULL,
     description VARCHAR(250),
     photo BLOB,
-    university VARCHAR(100)
+    university_id INT,
+	CONSTRAINT user_fk_university FOREIGN KEY (university_id) REFERENCES University (university_id)
 );
 
 drop table if exists Follow;
 create table Follow (
-	follower_id INT,
-    followee_id INT,
+	follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
     CONSTRAINT follower_fk FOREIGN KEY (follower_id) REFERENCES User (user_id),
     CONSTRAINT followee_fk FOREIGN KEY (followee_id) REFERENCES User (user_id)
 );
@@ -48,8 +54,8 @@ create table Appliance (
 
 drop table if exists Recipe_appliance;
 create table Recipe_appliance (
-	appliance_id INT,
-    recipe_id INT,
+	appliance_id INT NOT NULL,
+    recipe_id INT NOT NULL,
     CONSTRAINT recipe_appliance_fk_appliance FOREIGN KEY (appliance_id) REFERENCES Appliance (appliance_id),
     CONSTRAINT recipe_appliance_fk_recipe FOREIGN KEY (recipe_id) REFERENCES Recipe (recipe_id)
 );
@@ -63,8 +69,8 @@ create table Ingredient (
 
 drop table if exists Recipe_ingredient;
 create table Recipe_ingredient (
-	ingredient_id INT,
-    recipe_id INT,
+	ingredient_id INT NOT NULL,
+    recipe_id INT NOT NULL,
     amount INT NOT NULL,
     unit VARCHAR(20) NOT NULL,
     optional TINYINT NOT NULL DEFAULT FALSE,
@@ -80,8 +86,8 @@ create table Tag (
 
 drop table if exists Recipe_tag;
 create table Recipe_tag (
-	tag_id INT,
-    recipe_id INT,
+	tag_id INT NOT NULL,
+    recipe_id INT NOT NULL,
     CONSTRAINT recipe_tag_fk_tag FOREIGN KEY (tag_id) REFERENCES Tag (tag_id),
     CONSTRAINT recipe_tag_fk_recipe FOREIGN KEY (recipe_id) REFERENCES Recipe (recipe_id)
 );
@@ -89,8 +95,8 @@ create table Recipe_tag (
 
 drop table if exists Rating;
 create table Rating (
-	user_id INT,
-    recipe_id INT,
+	user_id INT NOT NULL,
+    recipe_id INT NOT NULL,
     rating INT NOT NULL,
     rating_date DATETIME NOT NULL,
     CONSTRAINT rating_fk_user FOREIGN KEY (user_id) REFERENCES User (user_id),
@@ -99,21 +105,22 @@ create table Rating (
 
 drop table if exists Save;
 create table Save (
-	user_id INT,
-    recipe_id INT,
+	user_id INT NOT NULL,
+    recipe_id INT NOT NULL,
     date_liked DATETIME NOT NULL,
     CONSTRAINT save_fk_user FOREIGN KEY (user_id) REFERENCES User (user_id),
     CONSTRAINT save_fk_recipe FOREIGN KEY (recipe_id) REFERENCES Recipe (recipe_id)
 );
 
-
 drop table if exists Pantry;
 create table Pantry (
-	user_id INT,
-    ingredient_id INT,
+	user_id INT NOT NULL,
+    ingredient_id INT NOT NULL,
     CONSTRAINT pantry_fk_user FOREIGN KEY (user_id) REFERENCES User (user_id),
     CONSTRAINT pantry_fk_ingredient FOREIGN KEY (ingredient_id) REFERENCES Ingredient (ingredient_id)
 );
+
+
 
 
 
